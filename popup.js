fd7c7268aa5606161c399be94d117c33da5cbc62
Fredy <fwhatley@frontlineed.com>
2017-02-05 19:05:28 -0500

    add filesaver js lib, rename everything to TC to MS Test Builder

--- popup.js
+++ popup.js
@@ -1,6 +1,23 @@
 
 $(function(){
-    $('#name').keyup(function(){
-        $('#greeting').text('Hi, ' + $('#name').val() + '!');
+
+    // helpers
+    var getDate_YYYYMMDDHHMM = function(){
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth();
+        var day = date.getDay();
+        var hr = date.getHours();
+        var min = date.getMinutes();
+        var dateString = "" + year + month + day + hr + min;
+        return dateString;
+    };
+
+
+    // listeners
+    $('#download').click(function(){
+        //alert("Hi");
+        var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, getDate_YYYYMMDDHHMM() + ".playlist");
     });
 });
